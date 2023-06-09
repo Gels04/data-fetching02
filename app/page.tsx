3bd75a45ffb01e481ff7f080dd33a10d77ba864b
Gels04 <gelcira2004@outlook.com>
2023-06-09 15:34:39 +0100

    can now add users and filter search users

--- app/page.tsx
+++ app/page.tsx
@@ -3,39 +3,108 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
 import getAllUsers from '@/lib/getAllUsers'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useMemo, useRef, useState } from 'react'
 
-export default async function Home() {
+//cannot have an async component in react/next. If you want to fetch asynchronous data, must put that in an async function - see useEffect(), line 19
+export default function Home() {
 
-  const usersData: Promise<User[]> = getAllUsers()
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+  const [query, setQuery] = useState("")
 
-  const users = await usersData;
-  console.log(users)
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const userNameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
 
-  const content = users.map((user, id) => {
-    return(
-    <>
-      <Link href={`/${user.id}`} key={id} style={{fontSize: "25px"}}>{user.name}</Link>
+
+  useEffect(() => {
+    async function fetchData() {
+      const usersData: Promise<User[]> = getAllUsers()
+      const users = await usersData;
+      setAllUsers(users);
+    }
+
+    fetchData()
+  }, [])
+
+// when a user submits, it adds another user to the list - only adds the user if all fields have been provided
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+
+    e.preventDefault();
+
+    const value: User = {
+      name: nameInputRef.current!.value,
+      username: userNameInputRef.current!.value,
+      email: emailInputRef.current!.value,
+    }
+
+    setAllUsers((prev) => {
+      return [...prev, value]
+    })
+
+    nameInputRef.current!.value = ""
+    userNameInputRef.current!.value = ""
+    emailInputRef.current!.value = ""
+    
+  }
+
+    // filters list of users
+    // useMemo - only rerender this code when the `users` and `query` state changes - this ensures function still works despite large amounts of data
+  const filteredUsers = useMemo(() => {
+    return allUsers.filter((user) => {
+      return user.name.toLowerCase().includes(query.toLowerCase());
+  })}, [allUsers, query])
+
+
+  const content = filteredUsers.map((user) => {
+    
+    return (
+      <>
+      <h3><Link href={`/${user.id}`}>{user.name}</Link></h3>
       <br />
-    </>
+      </>
     )
   })
 
-  // function filter(e: ChangeEvent<HTMLInputElement>) {
-  //   // console.log(e.target.value);
-  //   const value = e.target.value.toLocaleLowerCase();
-  //   users.forEach((user) => {
-  //     const isVisible = user.name.toLocaleLowerCase().includes(value);
-  //     if(isVisible) console.log(user);
-  //   })
-  // }
+  const searchBar = (
+
+    <>
+      <label htmlFor="search"> Search
+      <input type="text" id="search" value={query} onChange={(e) => setQuery(e.target.value)}/>
+      </label>
+    </>
+
+  )
+
+
+  const form = (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="name">Name
+        <input required ref={nameInputRef} type="text" id='name'/>
+        <br />
+      </label>Username
+      <label htmlFor="username">
+        <input required ref={userNameInputRef} type="text" id='username'/>
+        <br />
+      </label>Email
+      <label htmlFor="email">
+        <input required ref={emailInputRef} type="text" id='email' />
+      </label>
+      <br />
+      <button type='submit'>Add</button>
+    </form>
+  )
+
 
   return (
+
     <>
-    <Link href={"/search"}>Search</Link>
+    {searchBar}
+    <br />
+    <br />
+    <br />
+    {form}
     <br />
     {content}
     </>
-    
   )
 }
